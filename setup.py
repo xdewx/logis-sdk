c9df14f5cd1b51b89ccb1f3d2ce44872a0549c7d
dlx <xdandyx@sina.cn>
2025-08-13 11:34:22 +0800

    fix(build): 修复find_packages问题

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="logis-sdk",
-    version="0.0.1",
+    version="0.0.3",
     description="络捷斯特 Python 模块合集",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -10,5 +10,5 @@ setup(
     author_email="present150608@sina.com",
     packages=find_packages(),
     install_requires=[],
-    python_requires=">=3.6",
+    python_requires=">=3.12",
 )
