918af67eab7856e14fad024b488f38724cb7c327
dlx <xdandyx@sina.cn>
2025-11-13 17:36:02 +0800

    feat(types): 增加echarts相关数据结构

---
+++ logis/data_type/echarts.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from pydantic import BaseModel, Field
+
+from .base import DEFAULT_PYDANTIC_MODEL_CONFIG
+
+
+class EchartsConfig(BaseModel):
+    option: dict = Field(default_factory=dict)
+    attrs: dict = Field(default_factory=dict)
+
+    model_config = DEFAULT_PYDANTIC_MODEL_CONFIG
+
+    def add_class(self, *class_list: str):
+        self.attrs.setdefault("class", []).extend(class_list)
+        return self
+
+    def add_css_style(self, *pairs: tuple[str, Any]):
+        self.attrs.setdefault("style", {}).update({k: v for k, v in pairs})
+        return self
