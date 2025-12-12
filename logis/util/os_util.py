bd3df7b0867f703eb5d9fdbfe076d43fddeb71de
dlx <xdandyx@sina.cn>
2025-12-12 19:04:45 +0800

    refactor: 兼容废弃注解

--- logis/util/os_util.py
+++ logis/util/os_util.py
@@ -1,9 +1,9 @@
 import logging
 from pathlib import Path
 from typing import List
-from warnings import deprecated
 
 import psutil
+from deprecated import deprecated
 
 
 @deprecated("请使用 find_all_process_on_port 替代")
