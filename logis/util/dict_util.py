f591ef308604b2641e4dc79edff84324734aa31a
dlx <xdandyx@sina.cn>
2025-12-19 11:42:10 +0800

    feat(dict): 新增counter比较方法

--- logis/util/dict_util.py
+++ logis/util/dict_util.py
@@ -2,7 +2,7 @@ __doc__ = """
 字典工具类
 """
 
-from typing import Any, Callable, Iterable, Tuple
+from typing import Any, Callable, Dict, Iterable, Tuple, Union
 
 cn_en_dict = dict(
     半径="radius",
@@ -55,3 +55,47 @@ def get_the_first_existent_key(obj: dict, *keys, default=None) -> Tuple[Any, Any
         if key in obj:
             return key, obj[key]
     return None, default
+
+
+from collections import Counter
+
+
+def compare_counter_progress(progress: Counter, target: Counter) -> int:
+    """
+    比较计数器的进度
+    主要是兼容低版本，高版本的Counter可以直接比较
+    """
+    p_keys = set(progress.keys())
+    t_keys = set(target.keys())
+    assert p_keys.issubset(t_keys), "progress must be a subset of target"
+    dk = len(p_keys) - len(target.keys())
+    if dk != 0:
+        return dk
+    dvs = []
+    for k in p_keys:
+        v1 = progress.get(k)
+        v2 = target.get(k)
+        dv = v1 - v2 if v1 != v2 else 0
+        if dv > 0:
+            dv = 1
+        elif dv < 0:
+            dv = -1
+        if dv != 0:
+            dvs.append(dv)
+
+    if len(dvs) == 0:
+        return 0
+
+    dv_sum = sum(dvs)
+
+    # 全部相等
+    if dv_sum == 0:
+        return 0
+    # 全部大
+    elif dv_sum == len(dvs):
+        return 1
+    # 全部小
+    elif dv_sum == -len(dvs):
+        return -1
+    # 有的大有的小就是小
+    return -1
