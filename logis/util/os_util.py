fdc1c65d903aa6c78f49976ed0b913d532996677
dlx <xdandyx@sina.cn>
2025-12-25 11:30:21 +0800

    feat(os): add get_disk_usage

--- logis/util/os_util.py
+++ logis/util/os_util.py
@@ -7,6 +7,34 @@ import psutil
 from ipa.decorator import deprecated
 
 
+def get_disk_usage(path: Union[str, Path], follow_symlink: bool = True):
+    """
+    获取指定路径的磁盘使用情况。
+
+    TODO: 增加缓存；迭代效率
+
+    Args:
+        path (Union[str, Path]): 要获取磁盘使用情况的路径。
+        follow_symlink (bool, optional): 是否跟随符号链接。默认值为 True。
+
+    Returns:
+        int: 路径占用的磁盘空间大小（字节）。
+    """
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError(f"path {path} not exists")
+
+    if path.is_symlink() and follow_symlink:
+        path = path.resolve()
+    if path.is_file():
+        byte_size = path.stat().st_size
+    elif path.is_dir():
+        byte_size = sum(get_disk_usage(f, follow_symlink) for f in path.glob("**/*"))
+    else:
+        raise ValueError(f"path {path} is not a file or dir")
+    return byte_size
+
+
 @deprecated("请使用 find_all_process_on_port 替代")
 def find_process_on_port(port: int):
     """
