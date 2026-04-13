2c96f597fbaac715798ae651dda5d5ea1c8c9134
dlx <xdandyx@sina.cn>
2026-04-13 14:59:10 +0800

    perf(sim): 搬运相关接口

--- logis/biz/sim/transport/iface/__init__.py
+++ logis/biz/sim/transport/iface/__init__.py
@@ -28,3 +28,33 @@ class ITransportBlueprint(IBlueprint):
         目的地
         """
         pass
+
+    def get_pickup_strategy(self, **kwargs):
+        """
+        取料位置选择策略
+        """
+        raise NotImplementedError("get_pickup_strategy not implemented")
+
+    def get_destination_strategy(self, **kwargs):
+        """
+        目的地选择策略
+        """
+        raise NotImplementedError("get_destination_strategy not implemented")
+
+    def get_path_finding_strategy(self, **kwargs):
+        """
+        路径规划策略
+        """
+        raise NotImplementedError("get_path_finding_strategy not implemented")
+
+    def get_agent_selection_strategy(self, **kwargs):
+        """
+        智能体选择策略
+        """
+        raise NotImplementedError("get_agent_selection_strategy not implemented")
+
+    def get_agent_idle_strategy(self, **kwargs):
+        """
+        智能体空闲策略
+        """
+        raise NotImplementedError("get_agent_idle_strategy not implemented")
