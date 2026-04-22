04b535a45d0071b99e3af39fbcd7ec8ca08f0b6a
yidaizongshi <1639631211@qq.com>
2026-04-22 09:41:07 +0800

    feat: 合并运输和货到人的相同属性

--- logis/biz/sim/transport/iface/__init__.py
+++ logis/biz/sim/transport/iface/__init__.py
@@ -1,8 +1,18 @@
 from abc import abstractmethod
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Optional, Union
 
+from logis.biz.sim.component import ComponentForm as Entity
+from logis.biz.sim.const import (
+    AgentIdleStrategyOption,
+    AgentSelectionStrategyName,
+    GoHomeStrategyFrequency,
+    OperationType,
+)
 from logis.biz.sim.iface.blueprint import IBlueprint
+from logis.data_type import Time
 from logis.iface import Shape
+from logis.util import none_if_in
+from logis.util.dict_util import get_the_first_existent_key
 
 if TYPE_CHECKING:
     from logis.biz.sim.storage import ICell, IRackGroup
@@ -13,6 +23,45 @@ class ITransportBlueprint(IBlueprint):
     搬运蓝图
     """
 
+    def __init__(self, entity: Entity, *args, **kwargs):
+        super().__init__(entity, *args, **kwargs)
+
+        # 位置相关
+        self.pickup_location_id: Optional[str] = none_if_in(
+            entity.properties.get("取料位置", entity.properties.get("取货位置")),
+            "-1",
+            "null",
+        )
+        self.destination_id: str = none_if_in(
+            entity.properties.get("目的地"), "-1", "null"
+        )
+        self.destination_selection_strategy = entity.properties.get("目的地选择策略")
+
+        # 资源相关
+        self.retrieval_location_selection_strategy = entity.properties.get(
+            "取料位置选择策略"
+        )
+        _, v = get_the_first_existent_key(
+            entity.properties, "搬运资源选择策略", "选择搬运资源策略"
+        )
+        self.agent_selection_strategy_name: Optional[AgentSelectionStrategyName] = v
+
+        # 时间相关
+        self.loading_time = Time.parse_str(entity.properties.get("装载时间", "0|秒"))
+        self.unloading_time = Time.parse_str(entity.properties.get("卸载时间", "0|秒"))
+        self._moving_speed = none_if_in(
+            entity.properties.get("移动速度"), "DefaultValue"
+        )
+
+        # 资源释放策略
+        release_str: str = entity.properties.get("释放资源后", "")
+        release_config = release_str.split("|")
+        self.after_release_resource: AgentIdleStrategyOption = (
+            release_config[0] if len(release_config) > 0 else ""
+        )
+        self.go_home_frequency: GoHomeStrategyFrequency = (
+            release_config[1].split(":")[1] if len(release_config) > 1 else ""
+        )
     @property
     @abstractmethod
     def pickup_location(self) -> Union["IRackGroup", "ICell", Shape, None]:
